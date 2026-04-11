0472dde6b82ab193b06637ad084a05f2af6affbb
Hasboulouch <hasboulouch@gmail.com>
2026-04-11 02:03:35 -0400

    menuetscriptchangementsamplescenegame

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    public Image fadeImage;
+    public GameObject title;
+    public GameObject menuPanel;
+    public float fadeDuration = 2f;
+    public string gameSceneName = "SampleScene";
+
+    void Start()
+    {
+        StartCoroutine(Sequence());
+    }
+
+    IEnumerator Sequence()
+    {
+        yield return StartCoroutine(FadeIn());
+        yield return new WaitForSeconds(2f);
+        yield return StartCoroutine(FadeOut());
+
+        title.SetActive(false);
+        menuPanel.SetActive(true);
+
+        yield return StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        Color c = fadeImage.color;
+
+        for (float t = 1f; t > 0f; t -= Time.deltaTime / fadeDuration)
+        {
+            c.a = t;
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        c.a = 0;
+        fadeImage.color = c;
+    }
+
+    IEnumerator FadeOut()
+    {
+        Color c = fadeImage.color;
+
+        for (float t = 0f; t < 1f; t += Time.deltaTime / fadeDuration)
+        {
+            c.a = t;
+            fadeImage.color = c;
+            yield return null;
+        }
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(FadeOutAndLoad());
+    }
+
+    IEnumerator FadeOutAndLoad()
+    {
+        Color c = fadeImage.color;
+
+        for (float t = 0f; t < 1f; t += Time.deltaTime / fadeDuration)
+        {
+            c.a = t;
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
